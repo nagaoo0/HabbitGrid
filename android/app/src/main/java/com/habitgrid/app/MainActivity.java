f993bc581086fde536c6c703c55c64507d9feaf8
count0 <mixaciric000@gmail.com>
2025-10-19 00:30:36 +0200

    Android App Test

---
+++ android/app/src/main/java/com/habitgrid/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.habitgrid.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
+import { Capacitor } from '@capacitor/core';
 import { Helmet } from 'react-helmet';
 import HomePage from './pages/HomePage';
 import HabitDetailPage from './pages/HabitDetailPage';
@@ -9,8 +10,10 @@ import LoginProvidersPage from './pages/LoginProvidersPage';
 import { Toaster } from './components/ui/toaster';
 
 function App() {
+  const isNative = Capacitor?.isNativePlatform?.() ?? false;
+  const RouterComponent = isNative ? HashRouter : BrowserRouter;
   return (
-    <Router>
+    <RouterComponent>
       <Helmet>
         <title>HabitGrid - Commit to yourself, one square at a time</title>
         <meta name="description" content="Track your habits with a beautiful GitHub-style contribution grid. Build streaks, visualize progress, and commit to yourself daily." />
@@ -26,7 +29,7 @@ function App() {
         </Routes>
         <Toaster />
       </div>
-    </Router>
+    </RouterComponent>
   );
 }
 
